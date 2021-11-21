06ea82062de88bb7871bc831341182c3d8970e8d
Sanjida Afrin <safrin2@myseneca.ca>
2021-11-21 11:27:43 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'cashRegisterApp',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
